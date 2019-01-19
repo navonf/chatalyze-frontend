031203cd30632a5d79a80f600751b87f5246cacb
Justin Powell <justinmpowell@knights.ucf.edu>
2019-01-19 05:10:35 -0500

    sockets

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react'
 import {Container, Row, Col, Card, CardBody, CardTitle, CardFooter, InputGroup, InputGroupAddon, Button, Input} from 'reactstrap';
+import io from 'socket.io-client';
 
 class Chat extends Component {
     constructor(props){
         super(props);
         this.state = {
-            name: '',
             message: '',
             messages: []
         }
+        this.socket = io('https://api-chatalyze.herokuapp.com');
+        this.socket.on('RECIEVE_MESSAGE', (data) => {
+            addMessage(data);
+        });
+        this.sendMessage = (e) => {
+            e.preventDefault();
+            this.socket.emit('SEND_MESSAGE', {
+                author: this.props.location.state.username,
+                message: this.state.message
+            })
+            this.setState({
+                message: ''
+            })
+        }
+
+        const addMessage = (data) => {
+            this.setState({
+                messages: [
+                    ...this.state.messages,
+                    data
+                ]
+            })
+        }
     }
+
+    grabInput = (e) => {
+        this.setState({
+            message: e.target.value
+        })
+    }
+
     render() {
         return (
             <Row>
@@ -17,12 +47,24 @@ class Chat extends Component {
                     <Card>
                         <CardBody>
                             <CardTitle>Hello, {this.props.location.state.username}</CardTitle>
+                            <hr />
+                            <div className="messages">
+                                {
+                                    this.state.messages.map((message, index) => {
+                                        return(
+                                            <div key={index}>
+                                                {message.author}: {message.message}
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
                         </CardBody>
                         <CardFooter>
                             <InputGroup>
-                                <Input placeholder="What's your message?" />
+                                <Input placeholder="What's your message?" onChange={(e) => this.grabInput(e)}/>
                                 <InputGroupAddon addonType="append">
-                                    <Button outline color="primary">Send</Button>
+                                    <Button outline color="primary" onClick={this.sendMessage}>Send</Button>
                                 </InputGroupAddon>
                             </InputGroup>
                         </CardFooter>
