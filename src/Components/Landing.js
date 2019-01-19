61ddccc9378fc7390279c16e4cd3e980ee4da0cd
Justin Powell <justinmpowell@knights.ucf.edu>
2019-01-19 00:22:28 -0500

    landing page is dope

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import ReactTyped from 'react-typed';
+import '../styles.css';
 
 const Landing = () => {
+  document.body.style.backgroundColor = '#202424';
+
   return (
     <div>
-      
+      <div class="mx-auto text-center landing">
+        <h1 class="mx-auto mb-0 landing-font">chatalyze
+          <span><img src="chat.png" height="100"/></span>
+        </h1>
+        <ReactTyped
+          strings={["an app to prevent bullying", "an app to prevent abuse", "an app to prevent violence", "an app to prevent harassment", "an app to create a better workplace."]}
+          typeSpeed={60}
+          backSpeed={90}
+          backDelay={1}
+          smartBackspace={true}
+          className="typedjs-font"
+        />
+      </div>
     </div>
   )
 }
