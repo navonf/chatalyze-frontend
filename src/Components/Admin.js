fe813cab764d0888bd612a8c5de127d328a731b5
Navon Francis <navonfrancis@gmail.com>
2019-01-19 07:56:16 -0500

    idk

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -7,25 +7,77 @@ class Admin extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      chatIds : []
+      chatData : {},
+      chatIds : [],
+      gotTranscript : false,
+      theName : ""
     }
   }
 
   componentDidMount() {
-    console.log("mount");
     // get all conversations.
     fetch('https://api-chatalyze.herokuapp.com/conversation/get_all_keys')
       .then((data) => data.json())
       .then((datajson) => {
-        var temp = [];
-        for(var arr in datajson) {
-          temp.push(datajson[arr]);
+        var temp = [];{}
+        for(var key in datajson) {
+          var arr = datajson[key].sort();
+          var newKey = "";
+
+          while(arr.length !== 0) {
+            newKey = (arr.length > 1) ? newKey + arr.shift() + "&" : newKey + arr.shift();
+          }
+
+          temp[newKey] = {};
+          var tempIds = this.state.chatIds;
+          tempIds.push(newKey);
+
+          this.setState({chatIds : tempIds});
         }
-        this.setState({chatIds : temp});
+        this.setState({chatData : temp});
+        console.log(this.state.chatData)
+      });
+  }
+
+  toggleTranscript = () => {
+    if(this.state.gotTranscript){
+      console.log("YEEY" + this.state.chatData[this.state.theName]);
+
+      return(
+        <p>
+          {JSON.stringify(this.state.chatData)}
+        </p>
+      )
+    }
+  }
+
+  getTranscript(names) {
+    const payload = {
+      "chatId" : names
+    }
+    fetch('https://api-chatalyze.herokuapp.com/conversation/get_single_transcript', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(names);
+        var tempChatData = {};
+        tempChatData[names] = data;
+        console.log("temp" + JSON.stringify(tempChatData));
+        this.setState({chatData : tempChatData});
+        console.log("now");
+        console.log(this.state.chatData);
+        this.setState({gotTranscript : !this.state.gotTranscript});
+        this.setState({theName : names});
       });
   }
 
   render() {
+
     return(
       <div style={{backgroundColor: "white", height:"100vh"}}>
         <Navbar className="bg-dark fixed-top">
@@ -37,12 +89,15 @@ class Admin extends Component {
 
         <ExpansionPanel footer={null} label="convos" expanderIcon={<MdKeyboardArrowDown size={35} />} >
           <ExpansionList>
-            <ExpansionPanel label="1" expanderIcon={<MdKeyboardArrowDown size={35} />} >
-            </ExpansionPanel>
-            <ExpansionPanel label="2" expanderIcon={<MdKeyboardArrowDown size={35} />} >
-            </ExpansionPanel>
-            <ExpansionPanel label="3" expanderIcon={<MdKeyboardArrowDown size={35} />} >
-            </ExpansionPanel>
+            {
+              this.state.chatIds.map((names, idx) => {
+                var nameList = names.split("&");
+                return <ExpansionPanel label={`${nameList.shift()} and ${nameList.shift()}`}
+                  key={names} expanderIcon={<MdKeyboardArrowDown size={35} />} onClick={() => this.getTranscript(names)} >
+                  {this.toggleTranscript()}
+                </ExpansionPanel>
+              })
+            }
           </ExpansionList>
         </ExpansionPanel>
 
